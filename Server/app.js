cc7d3ed85910272bb090400a0e422a169abcd363
Eric Montzka <emontzka@sbcglobal.net>
2020-12-02 10:49:00 -0600

    set up express

--- Server/app.js
+++ Server/app.js
@@ -1,17 +1,11 @@
 // const path = require("path"); // node.js module for working with directory paths
 const express = require("express");
-// const mongoose = require("mongoose")
-// const db = require("./db/db")
-// const postRouter = require("./Routes/post");
+const connectDB = require("./config/db");
 const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//    extended: false
-// }));
-// app.use(cors());
+
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 const PORT = process.env.PORT || 5000;
-// app.use("/api/posts", postRouter)
 app.listen(PORT, (req, res) => {
   console.log(`app is now listening to PORT ${PORT}`);
 });
